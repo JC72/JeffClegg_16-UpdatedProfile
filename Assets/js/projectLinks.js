7935cf25c7a6a477e92c0fad084a76bab151fe52
Jeff Clegg <jeffclegg@att.net>
2021-01-16 16:19:42 -0500

    Added new button, functionality and styles

--- Assets/js/projectLinks.js
+++ Assets/js/projectLinks.js
@@ -13,3 +13,19 @@ $("#btnDeploy6").on("click",function(){
 $("#btnDeploy1").on("click",function(){
     window.open("https://emilyah-01.github.io/Brew_Hopper");
 })
+
+$("#btnGitRep6").on("click",function(){
+    window.open("https://github.com/JC72/JeffClegg_06_WeatherDashboard");
+})
+
+$("#btnGitRep5").on("click",function(){
+    window.open("https://github.com/JC72/JeffClegg_04_CodeQuiz");
+})
+
+$("#btnGitRep4").on("click",function(){
+    window.open("https://github.com/JC72/JeffClegg_05_WorkDayScheduler");
+})
+
+$("#btnGitRep1").on("click",function(){
+    window.open("https://github.com/EmilyAH-01/Brew_Hopper");
+})
